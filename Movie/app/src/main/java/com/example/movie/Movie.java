b46a76478ea61d80fbb9bf6ffda6bc41b6319ee3
Tareq <tareaothman@gmail.com>
2020-07-01 18:58:01 +0300

    L

---
+++ Movie/app/src/main/java/com/example/movie/Movie.java
@@ -0,0 +1,10 @@
+package com.example.movie;
+
+public class Movie {
+
+    private String title;
+    private String MainActor;
+    private double Raite;
+    private int pgR;
+    private String genre;
+}
